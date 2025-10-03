a5f2784609cb7af737ab5ab7ffd7cdaba814854d
hichemchems <hichem.djacta@gmail.com>
2025-10-03 14:30:52 +0200

    `Update environment variables, database configuration, and Docker Compose settings`

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || 'easygestionpass',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     pool: {
